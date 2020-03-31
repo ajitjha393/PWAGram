cdc354600902b79e1776003d43a70e041fe5f932
Bishwajit jha <ajitjha393@gmail.com>
2020-03-31 18:44:24 +0530

    Registered background sync api and added fallback for unsupported browsers

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -5,6 +5,10 @@ var closeCreatePostModalButton = document.querySelector(
 );
 var sharedMomentsArea = document.querySelector("#shared-moments");
 
+var form = document.querySelector("form");
+var titleInput = document.querySelector("#title");
+var locationInput = document.querySelector("#location");
+
 function openCreatePostModal() {
   // createPostArea.style.display = "block";
 
@@ -119,4 +123,64 @@ if ("indexedDB" in window) {
   });
 }
 
-//
+// Listening to form submit
+
+const sendData = () => {
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+      id: new Date().toISOString(),
+      title: titleInput.value.trim(),
+      location: locationInput.value.trim(),
+      image:
+        "https://firebasestorage.googleapis.com/v0/b/pwagram-a86f0.appspot.com/o/sf-boat.jpg?alt=media&token=fd21de64-14bb-442b-9a9a-2ec7aa785c60"
+    })
+  }).then(res => {
+    console.log("Sent data ...", res);
+    updateUI();
+  });
+};
+
+form.addEventListener("submit", event => {
+  event.preventDefault();
+
+  if (titleInput.value.trim() === "" || locationInput.value.trim() === "") {
+    alert(" Please enter valid data! ");
+    return;
+  }
+
+  closeCreatePostModal();
+
+  // SyncManager api providesus with sync features
+  if ("serviceWorker" in navigator && "SyncManager" in window) {
+    navigator.serviceWorker.ready.then(sw => {
+      let post = {
+        id: new Date().toISOString(),
+        title: titleInput.value.trim(),
+        location: locationInput.value.trim()
+      };
+      writeData("sync-posts", post)
+        .then(() => {
+          return sw.sync.register("sync-new-post");
+        })
+        .then(() => {
+          let snackbarContainer = document.querySelector("#confirmation-toast");
+          let data = {
+            message: "Your post was saved for syncing 🙃 "
+          };
+
+          snackbarContainer.MaterialSnackbar.showSnackbar(data);
+        })
+        .catch(err => {
+          console.log("Something failed... ", err);
+        });
+    });
+  } else {
+    // This will directly send data to backend so that our app doesnt crash if backg sync not allowed(... remember one of the features of pwa)
+    sendData();
+  }
+});
